89fa2b3197d1ebeede14e184255ada7b50905c5f
Sami Hindi <lolipop66611@gmail.com>
2023-05-11 21:58:10 +0200

    Completed WR Exam revision

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,19 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Schuldocs</span>,
+  logo: <span>School Docs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/FujiwaraChoki/school/',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  search: {
+    placeholder: '🔍Schulinhalte durchsuchen',
+  },
+  docsRepositoryBase: 'https://github.com/FujiwaraChoki/school/tree/main',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'School Docs - © 2023 Sami Hindi',
   },
 }
 
